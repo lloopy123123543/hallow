686051a1e7ed70757e9d5104bdfbedd400136357
Парфёнов Игорь Николаевич <pl8422@C201N2.pl1.edu>
2022-02-19 15:42:34 +0500

    ss

---
+++ helloween-site-next-2-master/pages/index.js
@@ -0,0 +1,222 @@
+export default function Index() {
+  return (
+    <div className="site">
+          <div class="container">
+        <div class="header">
+            <div class="header_logo">
+                <div class="header_logo_for">
+                    <div class="header_logo_icon"></div>
+                    <div class="header_logo_text">Hallowin</div>
+                </div>
+
+            </div>
+            <div class="header_menu">
+                <div class="header_menu_text"><div class="header_menu_text_inside">Home</div></div>
+                <div class="header_menu_text"><div class="header_menu_text_inside">About</div></div>
+                <div class="header_menu_text"><div class="header_menu_text_inside">Candy</div></div>
+                <div class="header_menu_text"><div class="header_menu_text_inside">New</div></div>
+                <div class="header_menu_text"><div class="for_btn"><div class="text_in_btn">Support</div></div></div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="kost"></div>
+    </div>
+    <div class="container">
+        <div class="main_menu">
+            <div class="main_img">
+                <div class="main_img_img"></div>
+            </div>
+            <div class="text_place">
+                <div class="top_text"><div class="top_text_text">#TOP1 CHETO TAM</div></div>
+                <div class="middle_text">UOOOO  TRICK OR  TREAT </div>
+                <div class="bottom_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate consectetur provident asperiores ab reprehenderit, veritatis maiores earum voluptas eligendi magnam assumenda voluptates nesciunt! Nihil ea provident dolore expedita voluptatem aspernatur!</div>
+                <div class="main_menu_buttons">
+                    <div class="first_button"><div class="frst_button"><div class="ctr">Look now</div></div></div>
+                    <div class="second_button"><div class="cs">Track Record</div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="title">Faworite Scare Category</div>
+    </div>
+    <div class="container">
+        <div class="score">
+            <div class="score_blocks">
+                <div class="score_block">
+                    <div class="score_block_img"></div>
+                    <div class="score_block_title">Ghosts</div>
+                    <div class="score_block_text">Choose the ghosts!  The scarests there scare!</div>
+                </div>
+                <div class="score_block">
+                    <div class="score_block_img score_block_img1"></div>
+                    <div class="score_block_title">Ghosts</div>
+                    <div class="score_block_text">Choose the ghosts!  The scarests there scare!</div>
+                </div>
+                <div class="score_block">
+                    <div class="score_block_img score_block_img2"></div>
+                    <div class="score_block_title">Ghosts</div>
+                    <div class="score_block_text">Choose the ghosts!  The scarests there scare!</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="about">
+            <div class="about_text">
+                <div class="about_title">About hallowin  Night</div>
+                <div class="about_under_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet cum saepe provident placeat illo minima rem officiis delectus quidem. Maxime esse quam similique laboriosam, incidunt inventore temporibus consequatur iusto.Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime amet cum saepe provident placeat illo minima rem officiis delectus quidem. Maxime esse quam similique laboriosam, incidunt inventore temporibus consequatur iusto.</div>
+                <div class="about_btn"><div class="about_btn_text">Click</div></div>
+            </div>
+            <div class="about_img"></div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="title">What that!!!</div>
+    </div>
+    <div class="container">
+        <div class="Trick">
+            <div class="trick_blocks">
+                <div class="trick_block">
+                    <div class="trick_block_img"><div class="trk_size"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Candy</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text">15$</div>
+                </div>
+                <div class="trick_block">
+                    <div class="trick_block_img"><div class="trk_size trk_size2"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Candy</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text">15$</div>
+                </div>                <div class="trick_block">
+                    <div class="trick_block_img"><div class="trk_size trk_size3"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Candy</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text">15$</div>
+                </div>
+            </div>
+            <div class="trick_blocks">
+                <div class="trick_block">
+                    <div class="trick_block_img"><div class="trk_size trk_size4"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Candy</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text">15$</div>
+                </div>
+                <div class="trick_block">
+                    <div class="trick_block_img"><div class="trk_size trk_size5"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Candy</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text">15$</div>
+                </div>                <div class="trick_block">
+                    <div class="trick_block_img"><div class="trk_size trk_size6"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Candy</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text">15$</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container">
+        <div class="bat">
+            <div class="bat_centre">
+                <div class="bat_img"></div>
+                <div class="bat_text">
+                    <div class="bat_title">50% Discount  On New Product</div>
+                    <div class="bat_btn"><div class="bat_btn_text">Click</div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="title">New Arivals</div>
+    </div>
+    <div class="container">
+        <div class="arrivals">
+            <div class="arrivals_blocks">
+                <div class="arrival_block">
+                    <div class="arrival_new"><div class="arrival_new_text">New</div></div>
+                    <div class="arrival_block_img"><div class="arrival_block_img_omg"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Scary house</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">29.99</div></div>
+                </div>
+                <div class="arrival_block">
+                    <div class="arrival_new"><div class="arrival_new_text">New</div></div>
+                    <div class="arrival_block_img"><div class="arrival_block_img_omg arrival_block_img_omg2"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Scary house</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">29.99</div></div>
+                </div>
+                <div class="arrival_block">
+                    <div class="arrival_new"><div class="arrival_new_text">New</div></div>
+                    <div class="arrival_block_img"><div class="arrival_block_img_omg arrival_block_img_omg3"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">Scary house</div></div>
+                    <div class="trick_block_lines"><div class="line"></div></div>
+                    <div class="trick_block_text"><div class="trick_block_text_text">29.99</div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="title">Our news Letter</div>
+    </div>
+    <div class="container">
+        <div class="xd">
+            <div class="xd_title">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, atque? Consequuntur quos voluptatem debitis.</div>
+            <div class="xd_menu">
+                <div class="xd_menu_text"><div class="xd_menu_text_text">How are you?</div></div>
+                <div class="xd_menu_button">
+                    <div class="xd_btn"><div class="xd_btn_text">Click</div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="bot">
+            <div class="add"></div>
+            <div class="bot_col">
+                <div class="bot_col_title">About</div>
+                <div class="bot_col_default">SUS</div>
+                <div class="bot_col_default">We are 1</div>
+                <div class="bot_col_default">No you</div>
+                <div class="bot_col_default">FAQ</div>
+            </div>
+            <div class="bot_col">
+                <div class="bot_col_title">About</div>
+                <div class="bot_col_default">SUS</div>
+                <div class="bot_col_default">We are 1</div>
+                <div class="bot_col_default">No you</div>
+                <div class="bot_col_default">FAQ</div>
+            </div>
+            <div class="bot_col">
+                <div class="bot_col_title">About</div>
+                <div class="bot_col_default">SUS</div>
+                <div class="bot_col_default">We are 1</div>
+                <div class="bot_col_default">No you</div>
+                <div class="bot_col_default">FAQ</div>
+            </div>
+            <div class="bot_col">
+                <div class="bot_col_title">About</div>
+                <div class="bot_col_default">SUS</div>
+                <div class="bot_col_default">We are 1</div>
+                <div class="bot_col_default">No you</div>
+                <div class="bot_col_default">FAQ</div>
+            </div>
+            <div class="addd"></div>
+
+        </div>
+    </div>
+        <div className="site__menu">
+            Меню
+        </div>
+        <div className="site__content">
+          <div className="content__slider">
+              Слайдер
+          </div>
+        </div>
+      </div>
+    
+  )
+}
